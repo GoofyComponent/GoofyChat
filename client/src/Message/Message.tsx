9d46d3dfde7440875004ded2f6e83bdbe24bb98c
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2022-11-29 11:58:17 +0100

    Add the server for the react app

--- client/src/Message/Message.tsx
+++ client/src/Message/Message.tsx
@@ -1,40 +1,39 @@
-import { useState } from 'react'
-import './Message.css'
+import { useState } from "react";
+import "./Message.css";
 
 function Message() {
-    const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
 
-    function setNewMessage(msg:any) {
-        setMessages([
-          ...messages,
-          msg
-        ]);
-      }
+  function setNewMessage(msg: any) {
+    /* setMessages([...messages, msg]); */
+  }
 
-      function sendMessage(e:any) {
-        e.preventDefault();
-        const msg = {
-          username: e.target.username.value,
-          text: e.target.text.value
-        };
-        setNewMessage(msg);
-      }
+  function sendMessage(e: any) {
+    e.preventDefault();
+    const msg = {
+      username: e.target.username.value,
+      text: e.target.text.value,
+    };
+    setNewMessage(msg);
+  }
 
   return (
     <div>
       <h1>GoofyChat</h1>
       <div>
-        <p>
-          Choisissez avec qui vous voulez discuter
-        </p>
+        <p>Choisissez avec qui vous voulez discuter</p>
         <div>
           {/* Mettre ici la liste des personnes avec qui on peut discuter */}
-          <div><button>Alice</button></div>
-          <div><button>Bob</button></div>
+          <div>
+            <button>Alice</button>
+          </div>
+          <div>
+            <button>Bob</button>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Message
+export default Message;
